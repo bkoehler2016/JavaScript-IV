573ab77fa7b3bdc229667192e85b9e5f9c6de1bb
bkoehler2016 <bkoehler85282@gmail.com>
2019-10-10 20:47:46 -0700

    Fixed the Stretch problem to work correctly

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -25,7 +25,7 @@ class Instructor extends Person {
   }
   scoreCard(obj) {
     let plusOrMinus = Math.random() < 0.5 ? -1 : 1;
-    let numGrade = Math.floor(Math.random() * 25) * plusOrMinus;
+    let numGrade = Math.ceil(Math.random() * 25) * plusOrMinus;
     let finalNum = obj + numGrade;
     return finalNum;
 
@@ -75,16 +75,6 @@ class TeamLead extends Instructor {
   }
 
 }
-
-const student_one = new Student({
-  name: 'Ben',
-  age: 36,
-  location: 'Mesa',
-  previousBackground: 'Customer Service',
-  className: 'Javascript IV',
-  favSubjects: ['HTML', 'CSS', 'Javascript']
-});
-
 const instructor_one = new Instructor({
   name: 'Pace',
   age: 26,
@@ -92,7 +82,21 @@ const instructor_one = new Instructor({
   specialty: 'Javascript',
   favLanguage: 'Spanish',
   catchPhrase: 'Logitech is awesome'
+
 });
+
+const student_one = new Student({
+  name: 'Ben',
+  age: 36,
+  location: 'Mesa',
+  previousBackground: 'Customer Service',
+  className: 'Javascript IV',
+  favSubjects: ['HTML', 'CSS', 'Javascript'],
+  grade: instructor_one.scoreCard(75)
+
+});
+
+
 
 const teamLead_one = new TeamLead({
   name: 'Julie',
@@ -110,4 +114,4 @@ console.log(`${instructor_one.name} thinks ${instructor_one.catchPhrase}`);
 console.log(teamLead_one.standUp('WebPT11'));
 console.log(instructor_one.grade('Ben', 'Javascript IV'));
 console.log(`${teamLead_one.name} says ${teamLead_one.catchPhrase}`);
-console.log(student_one.graduate(instructor_one.scoreCard(instructor_one, 90)));
+console.log(student_one.graduate(instructor_one.scoreCard()));
